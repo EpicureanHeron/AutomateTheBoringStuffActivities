7221579f612fa6074d750b15565bb00118f8c275
Joe Thor <joethor1989@gmail.com>
2018-12-29 15:23:30 -0600

    chp 1 lesson 3

---
+++ 1 Python Basics/Lesson3.py
@@ -0,0 +1,17 @@
+print 'Hello World'
+
+print 'What is your name?'
+
+myName = raw_input()
+
+print 'Nice to meet you ' + myName
+
+print 'The lenght of your name is ' 
+
+print len(myName)
+
+print 'What is your age?'
+
+myAge = input()
+
+print 'You will be ' + str(int(myAge) + 1) + ' in one year.'
