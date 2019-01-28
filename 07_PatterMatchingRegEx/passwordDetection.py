56d33e294abc800b993a8e59d28736f5eebf2763
Joe Thor <joethor1989@gmail.com>
2019-01-27 21:25:08 -0600

    password secure works

--- 07_PatterMatchingRegEx/passwordDetection.py
+++ 07_PatterMatchingRegEx/passwordDetection.py
@@ -2,11 +2,23 @@ import re
 
 def passwordDetection(pw):
 
-    alphaLengthRegex = re.compile(r'^[A-z][0-9]{1,8}$')
+    alphaCaps = re.compile(r'[a-z]{1,8}')
+    alphaSmall = re.compile(r'[A-Z]{1,8}')
+    numberRe = re.compile(r'[0-9]{1,8}')
 
-    mo1 = alphaLengthRegex.search(pw)
-    print(mo1.group())
+    mo1 = alphaCaps.search(pw)
+    mo2 = alphaSmall.search(pw)
+    mo3 = numberRe.search(pw)
+
+    if mo1 == None or mo2 == None or mo3 == None or len(pw) > 8:
+        print(pw + " is not secure")
+
+    else:
+        print(pw + " is secure")
+
    
 passwordDetection('Test4')
+passwordDetection('1')
+passwordDetection('Eight555')
 #passwordDetection('')
-passwordDetection('BLahBlahB')
+passwordDetection('BLahBlahB000')
