022d1923d3d87e62e1963951ad090e8a83a86f15
Joe Thor <joethor1989@gmail.com>
2019-02-17 18:22:22 -0600

    madlib works, but regex could be added.

--- 08_ReadAndWritingFiles/madlib.py
+++ 08_ReadAndWritingFiles/madlib.py
@@ -1,15 +1,24 @@
 f = open('./madlib.txt', 'r+')
 
-typesOfWord = ['ADJECTIVE', 'NOUN', 'VERB', 'ADVERB']
+typesOfWord = ['ADJECTIVE', 'NOUN', 'VERB.', 'ADVERB']
 
 #filedata = f.read()
+
+newFile = ''
 
 for line in f:
     for word in line.split():
         if word in typesOfWord:
-           newWord = input('enter a ' + word + " ")
-           f = f.replace(word, newWord)
-
+           newWord = input('enter a ' + word + ": ")
+           newFile += newWord + ' '
+        
+        else:
+                newFile += word + ' '
+                
+f.truncate(0) 
+print(newFile)          
+f.write(newFile)
+print
 
 f.close()
 
