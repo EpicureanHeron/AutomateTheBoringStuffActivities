7a093af48453588445c05bab2341f35626f65639
Joe Thor <joethor1989@gmail.com>
2019-02-19 06:28:08 -0600

    renaming dates project chapter 9

---
+++ 09_OrganizingFiles/renamingDates.py
@@ -0,0 +1,6 @@
+# Say your boss emails you thousands of files with American-style dates (MM-DD-YYYY) in their 
+# names and needs them renamed to European-style dates (DD-MM-YYYY). 
+# This boring task could take all day to do by hand! Let’s write a program to do it instead.
+
+import os, shutil, re
+
