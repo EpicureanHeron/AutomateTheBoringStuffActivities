1d0717e298e82e3249dcdd357cb7c0b319bcc872
Joe Thor <joethor1989@gmail.com>
2019-03-01 19:17:09 -0600

    chapter 10

--- 09_OrganizingFiles/selectiveCopy/selectiveCopy.py
+++ 09_OrganizingFiles/selectiveCopy/selectiveCopy.py
@@ -1,9 +1,11 @@
-import os, shutil
+import os, shutil, re
 
 
 def selectiveCopy(fileExt):
 
+    # need to add regex to say AFTER the '.' in the file name, check to see if it matches the characters pased by user via fileExt.
     directory = os.path('./')
+
 
     for folderName, subfolders, filenames in os.walk(directory):
         print('The current folder is ' + folderName)
@@ -13,12 +15,8 @@ def selectiveCopy(fileExt):
     for filename in filenames:
         print('FILE INSIDE ' + folderName + ': '+ filename)
 
-
-
-
-
-
-
 userFileExt = input("enter the file extension (this will be the part after the '.' such that spam.txt would be 'txt'): ")
+
+
 
 selectiveCopy(userFileExt)
