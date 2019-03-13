fd5d3173e13f0712d114e63b84fd7a2223b8fc01
Joe Thor <joethor1989@gmail.com>
2019-03-13 06:33:41 -0500

    finsihed reading census data

--- 12_ExcelSpreadsheets/readCensusExcel.py
+++ 12_ExcelSpreadsheets/readCensusExcel.py
@@ -1,4 +1,9 @@
-import openpyxl, pprint
+import openpyxl
+import pprint
+
+#could be used to use python to get at what a pivot table does
+
+print('opening workbook...')
 
 wb = openpyxl.load_workbook('censuspopdata.xlsx')
 
@@ -6,3 +11,25 @@ sheet = wb.get_sheet_by_name('Population by Census Tract')
 
 countyData = {}
 
+print('reading rows...')
+
+for row in range(2, sheet.max_row + 1):
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+    # make sure the key for this state exists
+    # https://docs.python.org/3/library/stdtypes.html#dict.setdefault
+    countyData.setdefault(state, {})
+    # Make sure the key for this county in this state exists.
+
+    countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+    # Each row represents one census tract, so increment by one.
+    countyData[state][county]['tracts'] += 1
+    # Increase the county pop by the pop in this census tract.
+    countyData[state][county]['pop'] += int(pop)
+
+print('Writing results...')
+resultFile = open('census2010.py', 'w')
+resultFile.write('allData = ' + pprint.pformat(countyData))
+resultFile.close()
+print('Done.')
