b8163c0b81405ad1202f1c3a2cc3c05836dd1a17
Joe Thor <joethor1989@gmail.com>
2019-03-20 06:55:39 -0500

    added styling to multiplication table project

--- 12_ExcelSpreadsheets/multiplicationTable.py
+++ 12_ExcelSpreadsheets/multiplicationTable.py
@@ -1,12 +1,16 @@
 import openpyxl
+from openpyxl.styles import Font
 import sys
 
 wb = openpyxl.Workbook()
 sheet = wb.active
+fontObj1 = Font(name='Times New Roman', bold=True)
 
 for i in range(1, int(sys.argv[1]) + 1): 
     sheet['A' + str(i+1)] = i
+    sheet['A' + str(i+1)].font = fontObj1
     sheet[openpyxl.utils.get_column_letter(i+1) + '1'] = i
+    sheet[openpyxl.utils.get_column_letter(i+1) + '1'].font = fontObj1
 
 for row in range(2, sheet.max_row + 1):
     for column in range(2, sheet.max_column + 1):
