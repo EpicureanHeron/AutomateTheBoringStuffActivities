399e510ff036749f8875077f557bdbe76d90be99
Joe Thor <joethor1989@gmail.com>
2019-03-22 06:45:28 -0500

    spreadsheet copying first portion of cells and saving them

--- 12_ExcelSpreadsheets/blankRowInserter.py
+++ 12_ExcelSpreadsheets/blankRowInserter.py
@@ -1,16 +1,24 @@
 import openpyxl
 import sys
 
-rowNumber = sys.argv[1]
-blanksToInsert = sys.argv[2]
+rowNumber = int(sys.argv[1])
+blanksToInsert = int(sys.argv[2])
 spreadSheet = sys.argv[3]
 
+
+
 wb = openpyxl.load_workbook(spreadSheet)
+wb2 = openpyxl.Workbook()
 
 sheet = wb.active
+sheet2 = wb2.active
 
-for row in range(1, rowNumber):
+for row in range(1, rowNumber+1):
+    for column in range(1, sheet.max_column + 1):
+        sheet2[openpyxl.utils.get_column_letter(column) + str(row)].value = sheet[openpyxl.utils.get_column_letter(column) + str(row)].value
 
+
+wb2.save('copied.xlsx')
 # capture and save data from spreadsheet 
 
 # insert # of rows from blanksToInsert variable
