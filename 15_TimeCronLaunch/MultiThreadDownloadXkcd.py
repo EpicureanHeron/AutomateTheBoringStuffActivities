6541ed8293d21721b6d98028f2572981227b5f53
Joe Thor <joethor1989@gmail.com>
2019-07-09 07:00:44 -0500

    multi thread downloader works, passed a parser element and also added a try/except block to deal with errors

--- 15_TimeCronLaunch/MultiThreadDownloadXkcd.py
+++ 15_TimeCronLaunch/MultiThreadDownloadXkcd.py
@@ -11,23 +11,32 @@ def downloadXkcd(startComic, endComic):
         res = requests.get('http://xkcd.com/%s' % (urlNumber))
         res.raise_for_status()
 
-        soup = bs4.BeautifulSoup(res.text) 
+        soup = bs4.BeautifulSoup(res.text, 'html.parser') 
 
         # find the url for the comic image
         comicElem = soup.select('#comic img')
         if comicElem == []:
             print('could not find comic image')
         else:
-            comicUrl = comicElem[0].get('src')
-            # download the image
-            print('Downloading image %s...' % (comicUrl))
-            res = requests.get(comicUrl)
-            res.raise_for_status()
+            try:
+                comicUrl = 'http:' + comicElem[0].get('src')
 
-            imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
-            for chunk in res.iter_content(100000):
-                imageFile.write(chunk)
-            imageFile.close()
+                print('downloading image %s ' %(comicUrl))
+
+                res = requests.get(comicUrl)
+                res.raise_for_status()
+                imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+                for chunk in res.iter_content(100000):
+                    imageFile.write(chunk)
+
+                imageFile.close()
+
+            except requests.exceptions.MissingSchema:
+                prevLink = soup.select('a[rel="prev"]')[0]
+                url = 'http://xkcd.com' + prevLink.get('href')
+
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prevLink.get('href')
 
 
 downloadThreads = []
