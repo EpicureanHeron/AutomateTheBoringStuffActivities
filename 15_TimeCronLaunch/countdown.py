755ad2b7cdc94389b23e357806bbe16b6f9f05f6
Joe Thor <joethor1989@gmail.com>
2019-07-11 06:18:43 -0500

    couldn't get countdown to work exactly, but started chapter 16

--- 15_TimeCronLaunch/countdown.py
+++ 15_TimeCronLaunch/countdown.py
@@ -1,2 +1,9 @@
 import time, subprocess
 
+timeLeft = 10
+while timeLeft > 0:
+    print(timeLeft, end='')
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+
+subprocess.Popen('C:/Program Files (x86)/Notepad++/notepad++.exe', 'getToWork.txt')
