edc6a765a61e4fcafae592ed4755ce7dbb0f91a4
Joe Thor <joethor1989@gmail.com>
2019-07-14 16:59:13 -0500

    struggling to install pyzmail for whatever reason, get something about egg_info when python -m pip install pyzmail

--- 16_EmailandTexts/sendEmail.py
+++ 16_EmailandTexts/sendEmail.py
@@ -12,4 +12,8 @@ smtpObj.login('<email address>', pw)
 
 smtpObj.sendmail('<email address>', '<recipient address', 'Subject: testing!. \nI am testing this out')
 
+# The start of the email body string must begin with 'Subject: \n' for the subject line of the email. The '\n' newline character
+#      separates the subject line from the main body of the email.
+
+
 smtpObj.quit()
