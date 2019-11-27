bc8108bf0d45935326a775496e8a6927de4b5741
Joe Thor <joethor1989@gmail.com>
2019-11-27 10:21:32 -0600

    flipping images

--- 17_Images/excercise1.py
+++ 17_Images/excercise1.py
@@ -67,4 +67,15 @@ def ex6():
     thiccIm.save('thicc.png')
 
 
-ex6()
+
+
+def ex7():
+    catIm = Image.open('zophie.png')
+    # catIm.rotate(90).save('rotated90.png')
+    # catIm.rotate(180).save('rotated180.png')
+    # catIm.rotate(270).save('rotated270.png')
+    #catIm.rotate(6, expand=True).save('rotated6_expanded.png')
+    catIm.transpose(Image.FLIP_LEFT_RIGHT).save('horizontal_flip.png')
+    catIm.transpose(Image.FLIP_TOP_BOTTOM).save('vertical_flip.png')
+
+ex7()
