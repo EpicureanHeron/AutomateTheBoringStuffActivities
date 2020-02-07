e299cb08025f53a9dfccac24bda6f85a4f1aa06b
Joe Thor <joethor1989@gmail.com>
2020-02-07 06:17:14 -0600

    logo program working, needed to add dir /withLogo'

--- 17_Images/addLogo.py
+++ 17_Images/addLogo.py
@@ -9,29 +9,31 @@ logoIm = Image.open(logo_filename)
 logoWidth, logoHeight = logoIm.size
 
 for filename in os.listdir('.'):
-    if not(filename.endswith('.png') or filename.endswith('.jpg')) or filename == logo_filename:
+    print(filename)
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+        or filename == logo_filename:
         continue   # skip non-image files and the logo file itself
 
-        im = Image.open(filename)
-        width, height = im.size
+    im = Image.open(filename)
+    width, height = im.size
 
-        if width > square_fit_size and height > square_fit_size:
+    if width > square_fit_size and height > square_fit_size:
 
-            if width > height:
+        if width > height:
 
-                height = int((square_fit_size/width) * height)
+            height = int((square_fit_size/width) * height)
 
-                width = square_fit_size
+            width = square_fit_size
 
-            else:
-                width = int((square_fit_size / height) * width)
-                height = square_fit_size
+        else:
+            width = int((square_fit_size / height) * width)
+            height = square_fit_size
 
-            print('Resizing %s...' % (filename))
-            im = im.resize((width, height))   
+       # print('Resizing %s...' % (filename))
+    im = im.resize((width, height))   
 
-            print('adding logo to %s...' (filename))
+#  print('adding logo to %s...' (filename))
 
-            im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
-            im.save(os.path.join('withLogo', filename))
+    im.save(os.path.join('withLogo', filename))
